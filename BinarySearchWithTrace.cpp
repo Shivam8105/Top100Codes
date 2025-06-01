86d524f2a5b4f44a3dd1fa1849a6e8b64ad28a8f
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2025-06-01 21:38:55 +0530

    updated

--- BinarySearchWithTrace.cpp
+++ BinarySearchWithTrace.cpp
@@ -21,7 +21,6 @@ int binarySearch(int arr[], int n, int key) {
 
     return -1; // not found
 }
-
 int main() {
     int arr[] = {2, 4, 6, 8, 10, 12, 14};
     int n = sizeof(arr) / sizeof(arr[0]);
